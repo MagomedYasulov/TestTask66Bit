26d47c32e2893a9776a091e7b9e2436f74c53041
magomed <magomedyasulov8@gmail.com>
2025-06-03 01:03:13 +0300

    fix: не большие правки

--- TestTask66Bit/Extensions/WebApplicationExtentions.cs
+++ TestTask66Bit/Extensions/WebApplicationExtentions.cs
@@ -22,9 +22,9 @@ namespace TestTask66Bit.Extensions
             dbContex.Interns.Add(new Intern() 
             { 
                 InternshipId = 1, 
-                Name = "Magomed", 
-                Surname = "Yasulov",
-                Email = "magomed@gmail.com",
+                Name = "Имя 1", 
+                Surname = "Фамилия 1",
+                Email = "example1@gmail.com",
                 Gender = Enums.Gender.Male,
                 BirthDate = DateTime.UtcNow, 
                 ProjectId = 1,
@@ -33,9 +33,9 @@ namespace TestTask66Bit.Extensions
             dbContex.Interns.Add(new Intern()
             {
                 InternshipId = 1,
-                Name = "Magomed 2",
-                Surname = "Yasulov 2",
-                Email = "magomed2@gmail.com",
+                Name = "Имя 2",
+                Surname = "Фамилия 2",
+                Email = "example2@gmail.com",
                 Gender = Enums.Gender.Male,
                 BirthDate = DateTime.UtcNow,
                 ProjectId = 1,
@@ -44,9 +44,9 @@ namespace TestTask66Bit.Extensions
             dbContex.Interns.Add(new Intern()
             {
                 InternshipId = 1,
-                Name = "Magomed 3",
-                Surname = "Yasulov 3",
-                Email = "magomed3@gmail.com",
+                Name = "Имя 3",
+                Surname = "Фамилия 3",
+                Email = "example3@gmail.com",
                 Gender = Enums.Gender.Male,
                 BirthDate = DateTime.UtcNow,
                 ProjectId = 1,
